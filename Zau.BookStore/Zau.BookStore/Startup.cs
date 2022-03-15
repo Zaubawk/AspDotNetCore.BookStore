082f914f65b5df67684e5be6d1d8d186377dddd0
Zau Bawk <hpagazb@gmail.com>
2022-03-14 20:36:34 -0700

    Added MVC to application

--- Zau.BookStore/Zau.BookStore/Startup.cs
+++ Zau.BookStore/Zau.BookStore/Startup.cs
@@ -16,6 +16,7 @@ namespace Zau.BookStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +27,23 @@ namespace Zau.BookStore
                 app.UseDeveloperExceptionPage();
             }
 
+            //app.Use(async (context, next) =>
+            //{
+            //    await context.Response.WriteAsync("Hello from my first middleware. ");
+            //    await next();
+            //});
+
+            //app.Use(async (context, next) =>
+            //{
+            //    await context.Response.WriteAsync("Hello from my second middleware");
+            //});
+
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
